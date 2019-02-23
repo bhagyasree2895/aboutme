8d98be065a88ccecb26e63760bc17d8afa8c1c81
bhagyasree2895 <bhagyasree.c60@gmail.com>
2019-02-23 16:54:24 -0600

    Committing after few modifications

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ function testingfunc(a,b,c){
 QUnit.test('Testing calculating grades with several sets of inputs',function(assert){
     assert.equal(testingfunc(1,1,1),1,'passed with values with in range of grades as 1,1,1 output should be 1');
     assert.equal(testingfunc(4,4,4),4,'tested with values with in range of grades as 4,4,4 output should be 4');
-    assert.equal(testingfunc(7,8),0,'passed with only two values');
-    assert.equal(testingfunc(-1,3,4),0,'passed with one negative value');
-    assert.equal(testingfunc(0,0,0),0,'passed with no values');
+    assert.equal(testingfunc(7,8),0,'passed with only two values output should be 0');
+    assert.equal(testingfunc(-1,3,4),0,'passed with one negative value output should be 0');
+    assert.equal(testingfunc(0,0,0),0,'passed with no values output should be 0');
 });
